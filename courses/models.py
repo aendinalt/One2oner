8cdeec86a18a760a2e162f76456f9ed16968483c
lord <lordlemale@gmail.com>
2014-12-06 16:46:58 +0200

    hw10 - step 5

--- courses/models.py
+++ courses/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
+tech = (
+    ('Python', 'Python'),
+    ('Ruby', 'Ruby'),
+    ('JavaScript', 'JavaScript'),
+)
 
 class Course(models.Model):
 
-    technology = models.CharField(max_length=255)
+    technology = models.CharField(max_length=255, choices=tech)
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=500)
     instructor = models.CharField(max_length=255)
--- students/models.py
+++ students/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+packages = (
+    ('Standart', 'Standart'),
+    ('Gold', 'Gold'),
+    ('VIP', 'VIP'),
+)
+
 
 class Student(models.Model):
 
@@ -8,7 +14,7 @@ class Student(models.Model):
     date_of_birth = models.DateField(null=True)
     email = models.EmailField()
     phone = models.CharField(max_length=30)
-    package = models.CharField(max_length=15)
+    package = models.CharField(max_length=15, choices=packages)
 
     def __unicode__(self):
         return self.name + ' ' + self.surname

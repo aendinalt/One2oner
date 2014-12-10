a3aa911907248cc059a48df230ca0c6f89d233a7
Andrey Matukhno <andrey.matukhno@altexsoft.com>
2014-12-10 14:07:02 +0200

    register surveyer's models in Admin

--- surveyer/admin.py
+++ surveyer/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from surveyer.models import *
+
 
 # Register your models here.
+class QuestionInline(admin.TabularInline):
+    model = Survey.questions.through
+    extra = 1
+
+
+@admin.register(Survey)
+class SurveyAdmin(admin.ModelAdmin):
+    fields = ['name']
+    inlines = [QuestionInline]
+
+admin.site.register(Question)
+admin.site.register(Answer)
--- surveyer/models.py
+++ surveyer/models.py
@@ -1,4 +1,30 @@
 from django.db import models
 
-# Create your models here.
 
+# Create your models here.
+class Question(models.Model):
+    question_text = models.TextField(max_length=1000)
+
+    def __unicode__(self):
+        return Question.question_text[:50]
+
+
+class Survey(models.Model):
+    name = models.CharField(max_length=100)
+    questions = models.ManyToManyField(Question)
+
+    def __unicode__(self):
+        return Survey.name[:50]
+
+
+class Answer(models.Model):
+    mark = models.CharField(max_length=10, choices=(
+        ('G', ':-)'),
+        ('N', ':-|'),
+        ('B', ':-('),
+    ))
+    explanation = models.TextField(max_length=1000, null=True, blank=True)
+    question = models.ForeignKey(Question)
+
+    def __unicode__(self):
+        return Answer.mark + ' ' + Answer.question.__unicode__()

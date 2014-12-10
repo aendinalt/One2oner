c4cbeada5d2233ced7e92f729dd53741a1ca5662
lord <lordlemale@gmail.com>
2014-12-10 09:58:57 +0200

    make initial settings add base template ste / url to dashboard view

---
+++ One2oner.mm
@@ -0,0 +1,157 @@
+<map version="freeplane 1.2.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="One2Oner" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1417809282107"><hook NAME="MapStyle" zoom="2.143">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="3"/>
+<node TEXT="Pages" POSITION="right" ID="ID_934732698" CREATED="1417809287018" MODIFIED="1417812346478">
+<edge COLOR="#ff0000"/>
+<font BOLD="true"/>
+<node TEXT="Template" ID="ID_1429720396" CREATED="1417810972375" MODIFIED="1417812264941">
+<font BOLD="true"/>
+<node TEXT="Header - top menu" ID="ID_1031675876" CREATED="1417811461592" MODIFIED="1417812266477">
+<font BOLD="true"/>
+<node TEXT="Logo with href to Dashboard" ID="ID_568667218" CREATED="1417811478392" MODIFIED="1417812239021">
+<font BOLD="true"/>
+</node>
+<node TEXT="Planning" ID="ID_1521618065" CREATED="1417811537489" MODIFIED="1417812245373">
+<font BOLD="false"/>
+</node>
+<node TEXT="Reports" ID="ID_1449721740" CREATED="1417811526785" MODIFIED="1417811535632"/>
+<node TEXT="Calendar" ID="ID_1619343989" CREATED="1417811517169" MODIFIED="1417812230805">
+<font ITALIC="true"/>
+<node TEXT="Outlook sync" ID="ID_1222180831" CREATED="1417811751924" MODIFIED="1417812230810">
+<font ITALIC="true"/>
+</node>
+<node TEXT="Create event-file" ID="ID_1717764387" CREATED="1417811769285" MODIFIED="1417812230811">
+<font ITALIC="true"/>
+</node>
+</node>
+<node TEXT="Logout" ID="ID_216097287" CREATED="1417811509593" MODIFIED="1417812251452">
+<font BOLD="true"/>
+</node>
+</node>
+</node>
+<node TEXT="Login" ID="ID_832693272" CREATED="1417809315858" MODIFIED="1417812262397">
+<font BOLD="true"/>
+</node>
+<node TEXT="Dashboard" ID="ID_115142366" CREATED="1417809318674" MODIFIED="1417812260157">
+<font BOLD="true"/>
+<node TEXT="(Oncoming) One2one meetings in next 30 days" ID="ID_1943716738" CREATED="1417811155210" MODIFIED="1417812257707">
+<font BOLD="true"/>
+</node>
+<node TEXT="List of Employees with obsolete One2one records (i.e. 90 days left)" ID="ID_48675187" CREATED="1417811227875" MODIFIED="1417812257701">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="One2One" ID="ID_475227380" CREATED="1417809325626" MODIFIED="1417812297397">
+<font BOLD="true"/>
+<node TEXT="Add" ID="ID_417662574" CREATED="1417809335514" MODIFIED="1417812331949">
+<font BOLD="true"/>
+</node>
+<node TEXT="View" ID="ID_1829548370" CREATED="1417809338251" MODIFIED="1417812304686">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="Report" ID="ID_1705729282" CREATED="1417809341195" MODIFIED="1417809345450">
+<node TEXT="?" ID="ID_1632497331" CREATED="1417809346843" MODIFIED="1417809349345"/>
+<node TEXT="?" ID="ID_1056521995" CREATED="1417809347403" MODIFIED="1417809350858"/>
+</node>
+<node TEXT="User Profile" ID="ID_877067889" CREATED="1417809353899" MODIFIED="1417809376826"/>
+<node TEXT="Admin-side" ID="ID_13865223" CREATED="1417809364427" MODIFIED="1417809370083"/>
+</node>
+<node TEXT="Roles" POSITION="left" ID="ID_991632592" CREATED="1417809413364" MODIFIED="1417809415963">
+<edge COLOR="#00ff00"/>
+<node ID="ID_660261589" CREATED="1417809418036" MODIFIED="1417809722593"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <strike>Employee</strike>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="Interviewer" ID="ID_1205968222" CREATED="1417809424868" MODIFIED="1417809432555">
+<node TEXT="Add One2One records so that it will keep" ID="ID_458455254" CREATED="1417809732314" MODIFIED="1417810063485"/>
+<node TEXT="See my prev added One2One records" ID="ID_444453772" CREATED="1417810117728" MODIFIED="1417810151041"/>
+<node TEXT="Finds all One2one records for employee" ID="ID_805727759" CREATED="1417810151689" MODIFIED="1417810349170"/>
+<node TEXT="Chose which Records I want to see" ID="ID_139589989" CREATED="1417810496214" MODIFIED="1417810510629"/>
+<node TEXT="Mark point of Record as Important" ID="ID_1916351724" CREATED="1417810542087" MODIFIED="1417810776488"/>
+<node TEXT="Filter Records by Important mark" ID="ID_860878387" CREATED="1417810549671" MODIFIED="1417810791657"/>
+<node TEXT="Differ Important Records in records list" ID="ID_490050283" CREATED="1417810792188" MODIFIED="1417810818131"/>
+<node TEXT="One2One calendar" ID="ID_361163717" CREATED="1417811011247" MODIFIED="1417811029430">
+<node TEXT="On2one planning" ID="ID_1401680900" CREATED="1417811049088" MODIFIED="1417811058047"/>
+<node TEXT="One-one history per employee" ID="ID_1041299444" CREATED="1417811058552" MODIFIED="1417811072279"/>
+</node>
+<node ID="ID_1397050782" CREATED="1417810390533" MODIFIED="1417810425606"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <strike>Sent Record to Employee on verification</strike>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="Admin" ID="ID_1013996289" CREATED="1417809433428" MODIFIED="1417809435387"/>
+</node>
+</node>
+</map>

2b8af0aeb63611ea2e2b574d9d0c577566ddfb08
lord <lordlemale@gmail.com>
2014-12-23 20:00:57 +0200

    tmp

--- records/views.py
+++ records/views.py
@@ -20,22 +20,27 @@ def record_details(request, record_id):
 
 def record_edit(request, record_id):
     record = Record.objects.get(id=record_id)
-
-    #answers = Answer.objects.filter(record__id=record_id)
     title = 'Edit record "' + str(record.id) + '"'
     if request.method == 'POST':
         form = RecordForm(request.POST, instance=record)
-
+        answer_formset = AnswerFormSet(request.POST,
+                                       queryset=Answer.objects.
+                                       filter(record__id=record_id))
         if form.is_valid() and answer_formset.is_valid():
             record = form.save()
+            answer_formset.save()
             return redirect('record-details', record.id)
     else:
         form = RecordForm(instance=record)
-        answer_formset = AnswerFormSet(queryset=Answer.objects.filter(record__id=record_id))
+        answer_formset = AnswerFormSet(
+            queryset=Answer.objects.filter(record__id=record_id),
+            initial=[{
+               'record': record,
+            }])
 
     return render(request, 'records/edit.html', {
         'record': record,
-        'answers': answer_formset,
+        'answer_formset': answer_formset,
         'form': form,
         'title': title
         })

36ff891de3d5faf8bf9bbf9c1ac1ef7eb6c51135
Andrey Matukhno <andrey.matukhno@altexsoft.com>
2014-12-23 16:13:01 +0200

    temp

--- records/forms.py
+++ records/forms.py
@@ -1,8 +1,14 @@
 from django import forms
 from models import *
+from django.forms.models import modelformset_factory
+
+
+AnswerFormSet = modelformset_factory(Answer)
 
 
 class RecordForm(forms.ModelForm):
     class Meta:
         model = Record
+
+
 
--- records/views.py
+++ records/views.py
@@ -20,18 +20,22 @@ def record_details(request, record_id):
 
 def record_edit(request, record_id):
     record = Record.objects.get(id=record_id)
-    answers = Answer.objects.filter(record__id=record_id)
+
+    #answers = Answer.objects.filter(record__id=record_id)
     title = 'Edit record "' + str(record.id) + '"'
     if request.method == 'POST':
         form = RecordForm(request.POST, instance=record)
-        if form.is_valid():
+
+        if form.is_valid() and answer_formset.is_valid():
             record = form.save()
             return redirect('record-details', record.id)
     else:
         form = RecordForm(instance=record)
+        answer_formset = AnswerFormSet(queryset=Answer.objects.filter(record__id=record_id))
+
     return render(request, 'records/edit.html', {
         'record': record,
-        'answers': answers,
+        'answers': answer_formset,
         'form': form,
         'title': title
         })
